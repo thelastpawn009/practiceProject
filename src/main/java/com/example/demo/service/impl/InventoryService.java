cacf9cdd15ade166551ae3b99c6ad45fc5e1104c
thelastpawn009 <amrit009anand@gmail.com>
2026-06-02 21:03:55 +0530

    update repo

--- src/main/java/com/example/demo/service/impl/InventoryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.model.Product;
-
-
-public interface InventoryService {
-    Product addProduct(Product product);
-    Product removeProduct(Product product);
-}
